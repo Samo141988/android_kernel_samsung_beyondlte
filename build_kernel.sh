a237a820d1250b69791d14eb342c5675e657c80f
Samo141988 <154265838+Samo141988@users.noreply.github.com>
2023-12-25 02:30:14 +0300

    Update build_kernel.sh

--- build_kernel.sh
+++ build_kernel.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 
-export PLATFORM_VERSION=11
-export ANDROID_MAJOR_VERSION=r
+export PLATFORM_VERSION=13
+export ANDROID_MAJOR_VERSION=T
 export ARCH=arm64
 
-make ARCH=arm64 exynos9820-beyond2lte_defconfig
+make ARCH=arm64 exynos9820-beyondx_defconfig
 make ARCH=arm64 -j16
